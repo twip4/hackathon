3f151c1e3c8a2f737bc368506942eda28d53d916
twip4 <paulbaudinot@gmail.com>
2022-03-25 10:36:27 +0100

    connection

--- test.py
+++ test.py
@@ -1,16 +1,42 @@
-import socket
+import pygame, sys, socket, struct
+from pygame.locals import *
 
-hote = "www.paulbaudinot.fr"
+from random import randint, choice, seed
+from dataclasses import dataclass, field
+from typing import Optional, Callable, Any, List, Dict, Tuple, Set
+
+hote = "localhost"
 port = 5000
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((hote, port))
 print("Connection on".format(port))
 
-mystring = "JOIN TEST"
-b = mystring.encode('utf-8')
+def ERROR(msg: str) -> None:
+    print("[ERROR] {}".format(msg))
+    exit()
 
-socket.send(b)
+def send(socket, message):
+    socket.send(struct.pack("i", len(message)) + message)
 
-print("Close")
+def recv(socket):
+    try:
+        size = struct.unpack("i", socket.recv(struct.calcsize("i")))[0]
+        data = ""
+        while len(data) < size:
+            msg = socket.recv(size - len(data))
+            if not msg:
+                ERROR("Problème lors de la reception du socket {}".format(socket))
+            data += msg.decode()
+        return data
+    except:
+        exit()
+
+m = "JOIN|TEST"
+m = m.encode()
+
+send(socket, m)
+
+recv(socket)
+
 socket.close()
